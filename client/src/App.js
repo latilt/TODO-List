639d1e60fd72b968900a883245efa808f36a0307
latilt <latilt2@gmail.com>
2018-10-30 15:19:50 +0900

    [client] TODO List show

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button, List } from 'semantic-ui-react';
 import './App.css';
 
+const temp = [{title: "오늘의 할일", content: "내일의 할일", done: false, deadline: 11.5},
+              {title: "title2", content: "content2", done: false, deadline: 11.5},
+              {title: "title3", content: "content3", done: false, deadline: 11.5}];
+
 class App extends Component {
+
+  // title : string, content : string, done : bool, deadline : date,
+  state = { lists : temp}
+
   render() {
+    const { lists } = this.state;
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <List celled size="massive">
+          {lists.map((list, index) => 
+            <List.Item key={index}>
+              <List.Header>{list.title}</List.Header>
+              <List.Content>{list.content}</List.Content>
+            </List.Item>  
+          )}
+        </List>
       </div>
     );
   }
