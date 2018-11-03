307fa32a993c57089f7ac6fb679b1bd2d5573880
latilt <latilt2@gmail.com>
2018-11-03 20:43:32 +0900

    [V-1] ToDo-List

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import { Form, Button, List, Modal, Checkbox, Message, Select } from 'semantic-ui-react';
+import { Header, Form, Button, List, Modal, Checkbox, Message, Select } from 'semantic-ui-react';
 import './App.css';
 
 class App extends Component {
 
   // title : string, content : string, done : bool, deadline : date, priority: 1~5
-  state = { lists : [], title: '', content: '', editTitle: '', editContent: '', date: '', editDate: '', dateBool: true, editDateBool: true, priority: 3, editPriority: 0}
+  state = { lists : [], title: '', content: '', editTitle: '', editContent: '', date: '', editDate: '', dateBool: true, editDateBool: true, priority: 3, editPriority: 0, inputError: false, editError: false, errorMessage: ''}
 
   priorityList = [{key: 1, value: 1, text:'1 (Lowest)'},
                   {key: 2, value: 2, text:'2 (Low)'},
                   {key: 3, value: 3, text:'3 (Medium)'},
                   {key: 4, value: 4, text:'4 (High)'},
                   {key: 5, value: 5, text:'5 (Highest)'}];
-
+  errorMessage = {
+    title: '제목이 입력되지 않았습니다.',
+    content: '내용이 입력되지 않았습니다.',
+    date: '기한이 입력되지 않았습니다.'
+  }
   componentDidMount() {
     this.getToDoLists();
   }
@@ -39,20 +43,32 @@ class App extends Component {
   handleSubmit = () => {
     const { lists, title, content, date, dateBool, priority } = this.state;
     if(title.length === 0) {
-      return console.log("제목이 입력되지 않았습니다.");
+      this.setState({
+        inputError: true,
+        errorMessage: this.errorMessage.title
+      });
+      return;
     }
     if(content.length === 0) {
-      return console.log("내용이 입력되지 않았습니다.");
+      this.setState({
+        inputError: true,
+        errorMessage: this.errorMessage.content
+      });
+      return;
     }
     if(!dateBool && !date) {
-      return console.log("기한이 입력되지 않았습니다.");
+      this.setState({
+        inputError: true,
+        errorMessage: this.errorMessage.date
+      });
+      return;
     }
     const data = {title, content, done: false, deadline: dateBool ? null : date || null, priority};
     this.postToDoLists(data)
       .then(res => res.json())
       .then(res => {
         this.setState({
-          lists: [...lists, res], title: '', content: '', date: '', dateBool: true, priority: 3
+          lists: [...lists, res], title: '', content: '', date: '', dateBool: true, priority: 3, inputError: false
         });
       });
   }
@@ -107,13 +123,25 @@ class App extends Component {
   handleEdit = (index) => {
     const { lists, editTitle, editContent, editDate, editDateBool, editPriority } = this.state;
     if(editTitle.length === 0) {
-      return console.log("바꿀 제목이 입력되지 않았습니다.");
+      this.setState({
+        editError: true,
+        errorMessage: this.errorMessage.title
+      });
+      return;
     }
     if(editContent.length === 0) {
-      return console.log("바꿀 내용이 입력되지 않았습니다.");
+      this.setState({
+        editError: true,
+        errorMessage: this.errorMessage.content
+      });
+      return;
     }
     if(!editDateBool && !editDate) {
-      return console.log("바꿀 기한이 입력되지 않았습니다.");
+      this.setState({
+        editError: true,
+        errorMessage: this.errorMessage.date
+      });
+      return;
     }
     const id = lists[index].id;
     const data = {title: editTitle, content: editContent, deadline: editDateBool ? null : editDate || null, priority: editPriority};
@@ -127,7 +155,8 @@ class App extends Component {
         list.deadline = editDateBool ? null : editDate;
         list.priority = editPriority;
         this.setState({
-          lists: newList
+          lists: newList,
+          editError: false
         });
       });
   }
@@ -151,7 +180,7 @@ class App extends Component {
   }
 
   render() {
-    const { lists, title, content, editTitle, editContent, date, editDate, dateBool, editDateBool, priority, editPriority } = this.state;
+    const { lists, title, content, editTitle, editContent, date, editDate, dateBool, editDateBool, priority, editPriority, inputError, editError, errorMessage } = this.state;
     let day;
     const outDateList = lists.filter((list) => {
       const gap = this.calculateDate(list.deadline);
@@ -161,7 +190,9 @@ class App extends Component {
 
     return (
       <div className="App">
-        <Form size="massive" onSubmit={this.handleSubmit}>
+        <header>ToDo - List</header>
+        <div className="controller">
+        <Form onSubmit={this.handleSubmit} error={inputError}>
           <Form.Field>
             <label>제목</label>
             <Form.Input placeholder='Title...' name='title' value={title} onChange={this.handleChange} />
@@ -179,23 +210,35 @@ class App extends Component {
             <Checkbox toggle onChange={this.toggleCheck} checked={!dateBool} />
             <Form.Input disabled={dateBool} type='date' name='date' value={date} onChange={this.handleChange} />
           </Form.Field>
+          <Message error header="Missing Content" content={errorMessage} />
           <Button type='submit'>submit</Button>
         </Form>
 
-        <List celled size="massive">
+        {outDateList.length ? 
+          <Message>
+            <Message.Header>마감기한이 지난 ToDo들이 있습니다.</Message.Header>
+            {outDateList.map((val, index) =>
+              <p key={index}>
+                {val.title}
+              </p>
+              )}
+          </Message> : ""}
+        </div>
+
+        <div className="screen">
+        <List celled>
           {lists.map((list, index) => 
             <List.Item key={index} className={list.done ? 'done' : ''}>
               <List.Header>
-                [{list.priority}]
+                [{list.priority}]&nbsp;
                 {list.title}
-                {isNaN(day = this.calculateDate(list.deadline)) ? "" : day > 0 ? day + "일 지났습니다." : -day + "일 남았습니다."}
                 <Modal 
-                  trigger={<Button floated='right' onClick={this.clickEdit.bind(this, list)}>Edit</Button>}
+                  trigger={<Button icon='edit' floated='right' onClick={this.clickEdit.bind(this, list)} />}
                   closeIcon
                   >
                   <Modal.Header>Edit a TODO</Modal.Header>
                   <Modal.Content>
-                    <Form size="massive" onSubmit={this.handleEdit.bind(this, index)}>
+                    <Form onSubmit={this.handleEdit.bind(this, index)} error={editError}>
                       <Form.Field>
                         <label>제목</label>
                         <Form.Input placeholder='Title...' name='editTitle' value={editTitle} onChange={this.handleChange} />
@@ -205,34 +248,29 @@ class App extends Component {
                         <Form.Input placeholder='Content...' name='editContent' value={editContent} onChange={this.handleChange} />
                       </Form.Field>
                       <Form.Field>
+                        <label>우선순위</label>
+                        <Select options={this.priorityList} name='editPriority' value={editPriority} onChange={this.handleChange} selection />
+                      </Form.Field>
+                      <Form.Field>
                         <Checkbox toggle onChange={this.toggleCheckEdit} checked={!editDateBool} />
                         <label>기한</label>
                         <Form.Input disabled={editDateBool} type='date' name='editDate' value={editDate} onChange={this.handleChange} />
                       </Form.Field>
-                      <Form.Field>
-                        <label>우선순위</label>
-                        <Select options={this.priorityList} name='editPriority' value={editPriority} onChange={this.handleChange} selection />
-                      </Form.Field>
+                      <Message error header="Missing Content" content={errorMessage} />
                       <Button type='submit'>Edit</Button>
                     </Form>
                   </Modal.Content>
                 </Modal> 
-                <Button floated='right' onClick={this.clickDelete.bind(this, index)}>DEL</Button>
-                <Button floated='right' onClick={this.clickDone.bind(this, index)}>Done</Button>
+                <Button icon='delete' floated='right' onClick={this.clickDelete.bind(this, index)} />
+                <Button icon='check' floated='right' onClick={this.clickDone.bind(this, index)} />
               </List.Header>
               <List.Content>{list.content}</List.Content>
+              <List.Content>{isNaN(day = this.calculateDate(list.deadline)) ? "" : day > 0 ? day + "일 지났습니다." : -day + "일 남았습니다."}</List.Content>
+              
             </List.Item>  
           )}
         </List>
-        {outDateList.length ? 
-          <Message>
-            <Message.Header>마감기한이 지난 목록들이 있습니다.</Message.Header>
-            {outDateList.map((val, index) =>
-              <p key={index}>
-                {val.title}
-              </p>
-              )}
-          </Message> : ""}
+        </div>
       </div>
     );
   }
